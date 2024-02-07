9f786bc8a184d41bca840de09defb15e675c4059
DiabGarren <garrendiab@gmail.com>
2024-02-07 11:37:54 +0200

    Runtime changes

--- obj/Debug/net8.0/financeManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/financeManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("financeManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f666ef9b1b43ed04d175289169597d0a8120227e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff76147255f7c23bd68ac00f9ee1e51ceb1e2c42")]
 [assembly: System.Reflection.AssemblyProductAttribute("financeManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("financeManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
